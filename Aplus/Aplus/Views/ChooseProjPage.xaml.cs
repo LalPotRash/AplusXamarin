a5db53be3df53d3b6d53b6f83dd8587c97e3be27
LalPotRash <daniil.kanev1994@gmail.com>
2022-05-30 17:45:16 +0300

    MVVM model

---
+++ Aplus/Aplus/Views/ChooseProjPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChooseProjPage : ContentPage
+    {
+        public ChooseProjPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Project_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabsPage());
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddProjectPage());
+        }
+    }
+}
